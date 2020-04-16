2ac98d397fc5b25e60510cb5a51b7b36d4b11e06
Jason Githui <jasongithui@gmail.comgit config --global user.name jasongithuiomit config --globalgit user.mail jasongithui@gmail.com>
2020-04-16 05:42:38 +0300

    give proceed btn a function

--- js/script.js
+++ js/script.js
@@ -23,3 +23,70 @@ function Getpizza( name,size,crust,topping, total ){
   this.topping = topping;
   this.total = total;
 }
+// proceed button
+$(document).ready(function(){
+    // $("button.proceed").click(function(){
+    //   $("button.proceed").hide();
+    //   $("#information").hide();
+    //   $("div.choise").slideDown(1000);
+    // });
+    $("button.proceed").click(function(event){
+     let pname = $(".name option:selected").val();
+     let psize = $("#size option:selected").val();
+     let pcrust = $("#crust option:selected").val();
+     let ptopping = [];
+     $.each($("input[name='toppings']:checked"), function(){            
+         ptopping.push($(this).val());
+     });
+     console.log(ptopping.join(", "));
+  
+     switch(psize){
+      case "0":
+        price =0;
+      break;
+      case "large":
+         price = 1200;
+         console.log(price);
+       break;
+       case "medium":
+         price = 850;
+         console.log("The price is "+price);
+       break;
+       case "small":
+         price = 600;
+         console.log(price);
+       default:
+         console.log("error"); 
+     }
+     switch(pcrust){
+        case "0":
+          crust_price = 0;
+        break;
+        case "Crispy":
+          crust_price = 200;
+        break;
+        case "Stuffed":
+          crust_price = 250;
+        break;
+        case "Gluten-free":
+          crust_price = 180;
+        break;
+        default:
+          console.log("No price"); 
+      }
+      let topping_value = ptopping.length*50;
+      console.log("toppins value" + topping_value);
+  
+      if((psize == "0") && (pcrust == "0")){
+        console.log("nothing selected");
+        $("button.proceed").show();
+        $("#information").show();
+        $("div.choise").hide();
+        alert("Please select pizza size and crust"); 
+      }
+      else{
+        $("button.proceed").hide();
+        $("#information").hide();
+        $("div.choise").slideDown(1000);
+      }
+  
